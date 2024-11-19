9a9de8b9b23f11568be0e06f62548d44bcc50a60
Rabia <yarr2659@gmail.com>
2024-11-19 13:58:37 -0600

    first initial commit

---
+++ src/C01_MyCode.java
@@ -0,0 +1,21 @@
+public class C01_MyCode {
+
+    public static void main(String[] args) {
+
+
+        int a =1;
+        int b=4;
+        System.out.println(a+b);
+        System.out.println("Version 01");
+
+        System.out.println("New Line");
+
+
+        //New comment
+        //This is new line of code
+
+        //Main
+        System.out.println("Version 02");
+
+    }
+}
---
+++ src/C02_NewClass.java
@@ -0,0 +1,5 @@
+public class C02_NewClass {
+    public static void main(String[] args) {
+        System.out.println("Version 04");
+    }
+}
