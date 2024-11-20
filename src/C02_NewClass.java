debe54671d8a025359ce363f7f4a32a427f4eb62
Rabiaay <yarr2659@gmail.com>
2024-11-19 22:12:04 -0600

    5

--- src/C02_NewClass.java
+++ src/C02_NewClass.java
@@ -2,5 +2,7 @@ public class C02_NewClass {
     public static void main(String[] args) {
         System.out.println("Version 03");
         //This line was made on GitHub
+        //v5 by GitHub
+        
     }
 }
